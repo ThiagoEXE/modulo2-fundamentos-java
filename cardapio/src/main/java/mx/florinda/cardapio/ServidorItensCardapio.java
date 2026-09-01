475633c39ed8ee221ca51c412c6c37ee15697030
ThiagoEXE <thiagob.oliveira@ucsal.edu.br>
2026-09-01 16:35:39 -0300

    Adição de try para classe Closeable no servidor

--- cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapio.java
+++ cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapio.java
@@ -1,35 +1,47 @@
 package mx.florinda.cardapio;
 
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpServer;
+
 public class ServidorItensCardapio {
 
-    static void main() throws IOException {
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(8080);
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(8000);
         HttpServer httpServer = HttpServer.create(inetSocketAddress, 0);
 
         httpServer.createContext("/itensCardapio.json", exchange -> {
 
-            Path path = Path.of("itensCardapio.json");
-            String json = Files.readString(path);
-            byte[] bytes = json.getBytes();
+            try {
 
-            Headers responseHeaders = exchange.getResponseHeaders();
-            responseHeaders.add("Content-type", "application/json; charset=UTF-8");
+                Path path = Path.of("itensCardapio.json");
+                String json = Files.readString(path);
+                byte[] bytes = json.getBytes();
 
-            exchange.sendResponseHeaders(200, bytes.length);
+                Headers responseHeaders = exchange.getResponseHeaders();
+                responseHeaders.add("Content-type", "application/json; charset=UTF-8");
 
-            OutputStream responseBody = exchange.getResponseBody();
-            responseBody.write(bytes);
+                exchange.sendResponseHeaders(200, bytes.length);
+
+                try (OutputStream responseBody = exchange.getResponseBody()) {
+
+                    responseBody.write(bytes);
+                    responseBody.flush();
+                }
+            } catch (IOException e) {
+                exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=UTF-8");
+                exchange.sendResponseHeaders(500, 0);
+                try (OutputStream responseBody = exchange.getResponseBody()) {
+                    responseBody.write(e.getMessage().getBytes());
+                }
+            }
         });
-        System.out.println("Subiu servidor http!");
+        System.out.println("Subiu servidor http! URL: http://localhost:8000/itensCardapio.json");
         httpServer.start();
 
     }
